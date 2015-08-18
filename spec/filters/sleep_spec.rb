431e4d6e079216dfcfef678f80de3d1ba9daaff4
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-18 13:31:15 +0200

    add basic test for the sleep filter
    Fixes #2

--- spec/filters/sleep_spec.rb
+++ spec/filters/sleep_spec.rb
@@ -1,5 +1,51 @@
-require "logstash/devutils/rspec/spec_helper"
-require 'logstash/filters/sleep'
+# encoding: utf-8
+require_relative "../spec_helper"
+require "logstash/plugin"
+require "logstash/event"
 
 describe LogStash::Filters::Sleep do
+
+  let(:time) { 1 }
+  subject { LogStash::Filters::Sleep.new("time" => time) }
+
+  let(:properties) { {:name => "foo"} }
+  let(:event)      { LogStash::Event.new(properties) }
+
+  it "should register without errors" do
+    plugin = LogStash::Plugin.lookup("filter", "sleep").new("time" => time)
+    expect { plugin.register }.to_not raise_error
+  end
+
+  describe "sleep for a given time" do
+
+    let(:time) { 5 }
+
+    before(:each) do
+      subject.register
+    end
+
+    it "should sleep for N seconds and continue" do
+      expect(subject).to receive(:sleep).with(5)
+      subject.filter(event)
+    end
+
+    context "when using every N events" do
+
+      let(:messages) { 20 }
+      let(:every) { 5 }
+      subject { LogStash::Filters::Sleep.new("time" => time, "every" => every ) }
+
+      before(:each) do
+        subject.register
+      end
+
+      it "should sleep for N seconds and continue" do
+        expect(subject).to receive(:sleep).with(5).exactly(4).times
+        messages.times do
+          subject.filter(event)
+        end
+      end
+
+    end
+  end
 end
